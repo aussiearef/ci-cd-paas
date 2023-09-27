f805d3a8baa7e79ddbb240ce9a37325f35c0783f
aussiearef <arefkr@gmail.com>
2023-09-28 08:06:45 +1000

    Added files

---
+++ create-web-dir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+web_root="/var/www"
+
+# Check if /var/www directory exists
+if [ ! -d "$web_root" ]; then
+    # If it doesn't exist, create it
+    sudo mkdir -p "$web_root"
+    sudo chown -R ubuntu:ubuntu "$web_root" # change ubuntu:ubuntu to your user:group
+fi
+
+echo "The /var/www directory is ready."
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet /var/www/ci-cd-demo.dll
