36f72904f0fee665f82b5f26edfa3c58e1f64130
aussiearef <arefkr@gmail.com>
2023-09-28 13:04:28 +1000

    Set the default port to 80

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/www
-sudo dotnet ci-cd-demo.dll --urls "http://*:8080" # listen on all network interfaces
+sudo dotnet ci-cd-demo.dll --urls "http://*:80" # listen on all network interfaces
