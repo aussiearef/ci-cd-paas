bc0f05c35b088ed3ad482c54e62f5b01a9e72df5
aussiearef <arefkr@gmail.com>
2023-09-28 13:23:36 +1000

    remove all files before deploy

--- create-web-dir.sh
+++ create-web-dir.sh
@@ -9,4 +9,6 @@ if [ ! -d "$web_root" ]; then
     sudo chown -R ubuntu:ubuntu "$web_root" # change ubuntu:ubuntu to your user:group
 fi
 
+rm -f /var/www/*
+
 echo "The /var/www directory is ready."
